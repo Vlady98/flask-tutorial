c142cd14bed5f3e14afe24c65039adcfcc1e4a2e
Diego Vladimir Romecin <romecin.duarte.diego@gmail.com>
2022-10-12 21:14:30 -0400

    [FIX] refactory about models and forms

--- app/main/views.py
+++ app/main/views.py
@@ -5,9 +5,9 @@ from .forms import NameForm
 from .. import db
 from ..models import User
 
+
 @main.route('/', methods=['GET', 'POST'])
 def index():
-
     form = NameForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.name.data).first()
@@ -20,8 +20,8 @@ def index():
             session['known'] = True
         session['name'] = form.name.data
         form.name.data = ''
-        return redirect(url_for('index'))
-        
+        return redirect(url_for('.index'))
+
     return render_template('index.html',
                             form=form, name=session.get('name'),
                             known=session.get('known', False),
